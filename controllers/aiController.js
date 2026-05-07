66e038d9f3f7b5b7d20d95f8215814364176fde3
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-07 18:15:48 +0530

    Add generate blog title api

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import { prisma } from "../config/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -42,6 +43,61 @@ export const generateArticle = async (req, res) => {
         prompt,
         content,
         type: "article",
+      },
+    });
+
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    res.json({ content });
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { prompt } = req.body;
+
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (plan !== "premium" && free_usage >= 10) {
+      return res.status(400).json({
+        message: "Limit reached. Upgrade to continue",
+      });
+    }
+
+    const response = await AI.chat.completions.create({
+      model: "gemini-3-flash-preview",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_completion_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+
+    await prisma.creations.create({
+      data: {
+        user_id: userId,
+        prompt,
+        content,
+        type: "blog-title",
       },
     });
 
